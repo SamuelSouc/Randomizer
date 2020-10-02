dee7833ffb94bf92df37dc094217935650acdc54
Jacka66er <samosouc@gmail.com>
2020-10-02 18:29:04 +0200

    Second commit

--- app/src/main/java/com/example/randomizer/MainActivity.java
+++ app/src/main/java/com/example/randomizer/MainActivity.java
@@ -6,19 +6,27 @@ import android.os.Bundle;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.Button;
 import android.widget.SeekBar;
 import android.widget.TextView;
 
+import java.util.Random;
+
 public class MainActivity extends AppCompatActivity {
+
+    private TextView textRolled = null;
+    private SeekBar barMax = null;
+    private TextView textMax = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        textMax = (TextView) findViewById(R.id.textMax);
+        textRolled = (TextView) findViewById(R.id.textRolled);
+        barMax = (SeekBar) findViewById(R.id.barMax);
         // nastavenie listenera na zmenu hodnoty seekbaru a updatnutie textu s max hodnotou
-        SeekBar barMax = (SeekBar) findViewById(R.id.barMax);
-        final TextView textMax = (TextView) findViewById(R.id.textMax);
+
         barMax.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
             @Override
             public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
@@ -36,6 +44,13 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void onRollClick(View v) {
-        
+        final int max = barMax.getProgress();
+        int rolled;
+        Random r = new Random();
+
+        rolled = r.nextInt(max + 1);
+
+        textRolled.setText(rolled + "");
     }
+
 }
