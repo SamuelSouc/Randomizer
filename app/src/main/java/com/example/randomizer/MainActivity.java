80a86065a4326a732f09a0fbe4b30b498ce49159
Jacka66er <samosouc@gmail.com>
2020-10-02 17:38:09 +0200

    First commit

---
+++ app/src/main/java/com/example/randomizer/MainActivity.java
@@ -0,0 +1,41 @@
+package com.example.randomizer;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.SeekBar;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // nastavenie listenera na zmenu hodnoty seekbaru a updatnutie textu s max hodnotou
+        SeekBar barMax = (SeekBar) findViewById(R.id.barMax);
+        final TextView textMax = (TextView) findViewById(R.id.textMax);
+        barMax.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                textMax.setText("Max = " + progress);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+            }
+        });
+    }
+
+    public void onRollClick(View v) {
+        
+    }
+}
